e83f6e54e6b474d2ead457a72221d7f69fe55ba8
Domenico Matteo <matteo.domenico@gmail.com>
2016-02-18 13:52:16 +0100

    tests: add tests for es6-modules

---
+++ test/es6Modules/templateLiterals.js
@@ -0,0 +1,107 @@
+'use strict';
+
+export default () => {
+
+  const one = '',
+    two = '',
+    member = { prop: 'bananas' },
+    noop = () => '';
+
+  this.expr = 'strawberries';
+
+  /**********************************************************************************************
+   *** HERE STARTS THE CONST WORLD **************************************************************
+   *********************************************************************************************/
+    
+  // Plain TemplateLiteral
+  const plain = t(`Template Literal`);
+
+  // { Identifier }
+  const oneVariable = t(`Template Literal with ${one} variable`);
+
+  // 2 { Identifier }
+  const twoVariables = t(`Template Literal with ${one} and ${two} variables`);
+
+  // { BinaryExpression }
+  const binaryExpr = t(`Template Literal with ${3-2} Binary Expression`);
+
+  // { CallExpression }
+  const callExpr = t(`Template Literal with ${noop()} Call Expression`);
+
+  // { CallExpression } using { TemplateLiteral }
+  const callExprAndTplLiterals = t(`Template Literal with ${t(`moar`)} Call Expression`);
+
+  // { CallExpression } using { TemplateLiteral } using { Identifier }
+  const callExprAndComplexLiterals = t(`Template Literal with ${t(`moar ${one}`)} Call Expression`);
+
+  // { MemberExpression }
+  const memberExpression = t(`Template Literal with ${member.prop} Member Expression`);
+
+  // { MemberExpression } using { ThisExpression }
+  const thisExpression = t(`Template Literal with ${this.expr} Member Expression`);
+  
+  /**********************************************************************************************
+   *** HERE STARTS THE LET WORLD ****************************************************************
+   *********************************************************************************************/
+    
+  // Plain TemplateLiteral
+  let plain_let = t(`Template Literal`);
+
+  // { Identifier }
+  let oneVariable_let = t(`Template Literal with ${one} variable`);
+
+  // 2 { Identifier }
+  let twoVariables_let = t(`Template Literal with ${one} and ${two} variables`);
+
+  // { BinaryExpression }
+  let binaryExpr_let = t(`Template Literal with ${3-2} Binary Expression`);
+
+  // { CallExpression }
+  let callExpr_let = t(`Template Literal with ${noop()} Call Expression`);
+
+  // { CallExpression } using { TemplateLiteral }
+  let callExprAndTplLiterals_let = t(`Template Literal with ${t(`moar`)} Call Expression`);
+
+  // { CallExpression } using { TemplateLiteral } using { Identifier }
+  let callExprAndComplexLiterals_let = t(`Template Literal with ${t(`moar ${one}`)} Call Expression`);
+
+  // { MemberExpression }
+  let memberExpression_let = t(`Template Literal with ${member.prop} Member Expression`);
+
+  // { MemberExpression } using { ThisExpression }
+  let thisExpression_let = t(`Template Literal with ${this.expr} Member Expression`);
+
+  /**********************************************************************************************
+   *** HERE STARTS THE VAR WORLD ****************************************************************
+   *********************************************************************************************/
+
+  // Plain TemplateLiteral
+  var plain_var = t(`Template Literal`);
+
+  // { Identifier }
+  var oneVariable_var = t(`Template Literal with ${one} variable`);
+
+  // 2 { Identifier }
+  var twoVariables_var = t(`Template Literal with ${one} and ${two} variables`);
+
+  // { BinaryExpression }
+  var binaryExpr_var = t(`Template Literal with ${3-2} Binary Expression`);
+
+  // { CallExpression }
+  var callExpr_var = t(`Template Literal with ${noop()} Call Expression`);
+
+  // { CallExpression } using { TemplateLiteral }
+  var callExprAndTplLiterals_var = t(`Template Literal with ${t(`moar`)} Call Expression`);
+
+  // { CallExpression } using { TemplateLiteral } using { Identifier }
+  var callExprAndComplexLiterals_var = t(`Template Literal with ${t(`moar ${one}`)} Call Expression`);
+
+  // { MemberExpression }
+  var memberExpression_var = t(`Template Literal with ${member.prop} Member Expression`);
+
+  // { MemberExpression } using { ThisExpression }
+  var thisExpression_var = t(`Template Literal with ${this.expr} Member Expression`);
+  
+};
+
+const t = (string) => string;
---
+++ test/tests.js
@@ -0,0 +1,61 @@
+'use strict';
+
+import templ18n from '../lib/templ18n';
+
+import Promise from 'bluebird';
+import expect from 'unexpected';
+
+const readFile = Promise.promisify(require("fs").readFile);
+const acorn = require('acorn-jsx/inject')(require('acorn'));
+
+describe('templ18n', () => {
+
+  const astObject = function(code, srcType = 'module') {
+    this.ast = {};
+    this.code = code || '';
+
+    this.getAST = () => {
+      this.ast = acorn.parse(this.code, {
+        sourceType: srcType,
+        plugins: { jsx: true }
+      });
+
+      return this;
+    };
+
+    this.astToString = () => {
+      this.code = JSON.stringify(this.ast, (key, value) => {
+        if (key === "start") return undefined;
+        else if (key === "end") return undefined;
+
+        else return value;
+      });
+
+      return this;
+    };
+  };
+
+  expect.addAssertion('<string> to be functionally equivalent <string>', function (expect, subject, value) {
+    const subjectAST = new astObject(subject);
+    const valueAST = new astObject(value);
+    expect(subjectAST.getAST().astToString().code, 'to equal', valueAST.getAST().astToString().code);
+  });
+
+  describe('es6-modules', () => {
+
+    const fixturesDir = './test/es6Modules';
+
+    it('should convert TemplateLiteral(s) to Literal(s)', () => {
+
+      Promise.all([
+        readFile(`${fixturesDir}/templateLiterals.js`, 'utf8'),
+        readFile(`${fixturesDir}/literals.js`, 'utf8')
+      ]).then((files) => {
+        const code = templ18n(files[0]);
+        return expect(code, 'to be functionally equivalent', files[1]);
+      });
+
+    });
+  });
+
+});
